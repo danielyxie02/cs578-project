6358aab707e4736afe1829197aadaca93d1cca78
Daniel Xie <danielyxie2002@gmail.com>
2023-12-04 21:10:46 -0500

    LeNet-300-100 (alternative LeNet model more common in pruning exercises)

--- models.py
+++ models.py
@@ -23,3 +23,16 @@ class LeNet_5(nn.Module):
         x = self.fc5(x)
 
         return x
+
+class LeNet_300_100(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.fc1 = nn.Linear(784, 300)
+        self.fc2 = nn.Linear(300, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x.view(-1, 784)))
+        x = F.relu(self.fc2(x))
+        return F.log_softmax(self.fc3(x), dim=1)
